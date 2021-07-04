ee0629828bd85efb02ba33a74a2c7b3774557908
LEE Ngeefah Ian <ionian1hx@gmail.com>
2021-07-04 19:03:24 +0800

    新增 OrderAPI。可下訂單、以 UserID 查訂單。

---
+++ InventoryManagementSystem/Controllers/Api/OrderApiController.cs
@@ -0,0 +1,93 @@
+﻿using InventoryManagementSystem.Models.EF;
+using InventoryManagementSystem.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Controllers.Api
+{
+
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class OrderApiController : ControllerBase
+    {
+
+        private InventoryManagementSystemContext _dbContext;
+
+        public OrderApiController(InventoryManagementSystemContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        /*
+         * OrderApi/MakeOrder
+         */
+        // 下訂單
+        [HttpPost]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public async Task<MakeOrderMessageResultModel> MakeOrder(Order order)
+        {
+            await _dbContext.AddAsync(order);
+            MakeOrderMessageResultModel msg = null;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+
+                // 下訂單成功
+                msg = new MakeOrderMessageResultModel(true);
+            }
+            catch(DbUpdateException e)
+            {
+                // 下訂單失敗
+                msg = new MakeOrderMessageResultModel(false);
+            }
+
+            return msg;
+        }
+
+        /*
+         * OrderApi/GetOrdersByUserId/{UserID}
+         */
+        // 以 UserID 查詢所有訂單（任何狀態的訂單都會查出來）
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("{id}")]
+        public async Task<OrderResultModel[]> GetOrdersByUserId(int id)
+        {
+            var results = await _dbContext.Orders
+                .Where(o => o.UserId == id)
+                .Select(o => new OrderResultModel
+                {
+                    OrderId = o.OrderId,
+                    UserId = o.UserId,
+                    EquipmentId = o.EquipmentId,
+                    Quantity = o.Quantity,
+                    EstimatedPickupTime = o.EstimatedPickupTime,
+                    Day = o.Day,
+                    OrderStatusId = o.OrderStatusId,
+                    OrderTime = o.OrderTime,
+
+                    EquipmentSn = o.Equipment.EquipmentSn,
+                    EquipmentName = o.Equipment.EquipmentName,
+                    Brand = o.Equipment.Brand,
+                    Model = o.Equipment.Model,
+                    UnitPrice = o.Equipment.UnitPrice,
+                    Description = o.Equipment.Description,
+
+                    Username = o.User.Username,
+
+                    StatusName = o.OrderStatus.StatusName
+                })
+                .ToArrayAsync();
+
+            return results;
+        }
+    }
+}
---
+++ InventoryManagementSystem/Models/ResultModels/OrderResultModel.cs
@@ -0,0 +1,36 @@
+﻿using InventoryManagementSystem.Models.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Models.ViewModels
+{
+    public class OrderResultModel
+    {
+        // From Order table
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public int EquipmentId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime EstimatedPickupTime { get; set; }
+        public int Day { get; set; }
+        public string OrderStatusId { get; set; }
+        public DateTime? OrderTime { get; set; }
+
+        // From Equipment table
+        public string EquipmentSn { get; set; }
+        public string EquipmentName { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public string Description { get; set; }
+
+        // From User table
+        public string Username { get; set; }
+        
+        // From OrderStatus table
+        public string StatusName { get; set; }
+
+    }
+}
