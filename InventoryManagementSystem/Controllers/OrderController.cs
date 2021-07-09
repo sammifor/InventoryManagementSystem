c7f6d71812adc717917233986a2d9bb092866be9
Sophia Chen <sophia83124@hotmail.com>
2021-07-09 14:41:39 +0800

    修正新增訂單頁面(成功送出封包)

--- InventoryManagementSystem/Controllers/OrderController.cs
+++ InventoryManagementSystem/Controllers/OrderController.cs
@@ -9,7 +9,7 @@ namespace InventoryManagementSystem.Controllers
     [Route("[controller]/[action]")]
     public class OrderController : Controller
     {
-        [HttpGet]
+        [HttpPost]
         [Route("{equipId}")]
         public IActionResult NewOrder(int equipId)
         {
---
+++ InventoryManagementSystem/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        public IActionResult Test2() 
+        {
+            return View();
+        }
+    }
+}
