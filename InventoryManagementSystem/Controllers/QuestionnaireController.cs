f5380e7fa48fc35e223f03c230692c9914d33598
allen955103@hotmail.com <allen955103@hotmail.com>
2021-07-12 00:06:06 +0800

    新增設備滿意度問卷View+Css

---
+++ InventoryManagementSystem/Controllers/QuestionnaireController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class QuestionnaireController : Controller
+    {
+        //滿意度調查問卷
+        public IActionResult questionnaire()
+        {
+            return View();
+        }
+    }
+}
