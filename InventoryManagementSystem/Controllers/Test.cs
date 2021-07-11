dcfc8902917e42a783457caec6d9d3559c768695
Sophia Chen <sophia83124@hotmail.com>
2021-07-11 11:13:42 +0800

    新增equipdAddAdmin頁面

---
+++ InventoryManagementSystem/Controllers/Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class Test : Controller
+    {
+        public IActionResult test()
+        {
+            return View();
+        }
+
+        public IActionResult test2()
+        {
+            return View();
+        
+        }
+    }
+}
