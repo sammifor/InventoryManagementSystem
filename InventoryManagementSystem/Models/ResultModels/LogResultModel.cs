5a0fa1e7f7c3a1098abbafa106088c00e5f19e1b
LEE Ngeefah Ian <ionian1hx@gmail.com>
2021-07-12 10:47:29 +0800

    新增以 itemID 查詢 ItemLog 的 API（ItemLogApi/GetLogsByItemId/）

---
+++ InventoryManagementSystem/Models/ResultModels/LogResultModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Models.ResultModels
+{
+    public class LogResultModel
+    {
+        public int ItemLogId { get; set; }
+        public int? OrderDetailId { get; set; }
+        public int? AdminId { get; set; }
+        public int ItemId { get; set; }
+        public string ConditionId { get; set; }
+        public string Description { get; set; }
+        public DateTime? CreateTime { get; set; }
+    }
+}
