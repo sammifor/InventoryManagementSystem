85c2ba3f00a9f1f7fa137924d645a36752699f0f
LEE Ngeefah Ian <ionian1hx@gmail.com>
2021-07-14 16:58:39 +0800

    歸還確認 API 已完成（OrderDetailApi/ReturnItemsCheck）

---
+++ InventoryManagementSystem/Models/ViewModels/ReturnItemsCheckViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Models.ViewModels
+{
+    public class ReturnItemsCheckViewModel
+    {
+        public int OrderDetailID { get; set; }
+        public bool FunctionsNormally { get; set; } // 是否運作正常？
+        public string Description { get; set; } // 詳細描述
+    }
+}
