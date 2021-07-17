73cc986855046530ae2d0308773047fededb252e
LEE Ngeefah Ian <ionian1hx@gmail.com>
2021-07-17 10:10:31 +0800

    新增管理員登入。現在管理員與使用者看到的 nav 不同。

--- InventoryManagementSystem/Controllers/AdminController.cs
+++ InventoryManagementSystem/Controllers/AdminController.cs
@@ -9,68 +9,65 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace InventoryManagementSystem.Controllers
 {
-    public class UserController : Controller, IHashPassword
+    public class AdminController : Controller, IHashPassword
     {
         private readonly InventoryManagementSystemContext _dbContext;
 
-        public UserController(InventoryManagementSystemContext dbContext)
+        public AdminController(InventoryManagementSystemContext dbContext)
         {
             _dbContext = dbContext;
         }
 
-        [HttpGet("login")]
+        [HttpGet("adminlogin")]
         public IActionResult Login()
         {
             return View();
         }
 
-        [HttpPost("login")]
+        [HttpPost("adminlogin")]
         public async Task<IActionResult> Authenticate(string username, string password)
         {
-            User user = await _dbContext.Users
-                .Where(u => u.Username == username)
+            Admin admin = await _dbContext.Admins
+                .Where(a => a.Username == username)
                 .FirstOrDefaultAsync();
 
-            if(user == null)
+            if(admin == null)
             {
                 return View("Login");
             }
 
-            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
             var hasher = this as IHashPassword;
-            byte[] hashedPassword = hasher.HashPasswordWithSalt(passwordBytes, user.Salt);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] hashedPassword = hasher.HashPasswordWithSalt(passwordBytes, admin.Salt);
 
-            if(hashedPassword.SequenceEqual(user.HashedPassword))
+            if(hashedPassword.SequenceEqual(admin.HashedPassword))
             {
 
                 List<Claim> claims = new List<Claim>();
-                claims.Add(new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString(), ClaimValueTypes.Integer32));
-                claims.Add(new Claim(ClaimTypes.Name, user.Username));
-                claims.Add(new Claim(ClaimTypes.Role, "user"));
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, admin.AdminId.ToString(), ClaimValueTypes.Integer32));
+                claims.Add(new Claim(ClaimTypes.Name, admin.Username));
+                claims.Add(new Claim(ClaimTypes.Role, "admin"));
                 ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
                 ClaimsPrincipal principal = new ClaimsPrincipal(identity);
                 await HttpContext.SignInAsync(principal);
-                return RedirectToAction("equipQryUser", "Equips");
+                return RedirectToAction("equipQryAdmin", "Equips");
             }
             else
             {
                 return View("Login");
             }
-
         }
 
-        [Authorize(Roles = "user")]
+        [Authorize(Roles = "admin")]
         public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
-
     }
 }
---
+++ InventoryManagementSystem/Models/Interfaces/IHashPassword.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Models.Interfaces
+{
+    interface IHashPassword
+    {
+        byte[] HashPasswordWithSalt(byte[] password, byte[] salt)
+        {
+            byte[] result = null;
+            using(SHA256 sha256hash = SHA256.Create()){
+                byte[] saltAndPassword = salt.Concat(password).ToArray();
+                result = sha256hash.ComputeHash(saltAndPassword);
+            }
+            return result;
+        }
+    }
+}
