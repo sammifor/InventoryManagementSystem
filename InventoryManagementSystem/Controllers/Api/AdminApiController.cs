d17617bb207b6c2c388445ecf6333708b4059ce4
LEE Ngeefah Ian <ionian1hx@gmail.com>
2021-07-18 14:04:34 +0800

    AdminApi 架構

---
+++ InventoryManagementSystem/Controllers/Api/AdminApiController.cs
@@ -0,0 +1,38 @@
+﻿using InventoryManagementSystem.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Controllers.Api
+{
+    [Route("api")]
+    [ApiController]
+    public class AdminApiController : ControllerBase
+    {
+        // 查詢所有 admin
+        [HttpGet("admin/{id}")]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> GetAdmin(int id)
+        {
+            return Ok(id);
+        }
+
+        // 新增 admin
+        [HttpPost("admin")]
+        [Consumes("application/json")]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> PostAdmin(PostAdminViewModel model)
+        {
+            return Ok(model.Username + " post");
+        }
+
+        // 修改 admin
+        //[HttpPut("admin")]
+        //public
+
+    }
+}
