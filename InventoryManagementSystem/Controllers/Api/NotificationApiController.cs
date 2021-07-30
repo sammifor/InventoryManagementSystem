3c5e689db22b33bbe17ebf02ada9253e1b8af39a
LEE Ngeefah Ian <ionian1hx@gmail.com>
2021-07-30 23:24:41 +0800

    逾期通知新增 LINE 通知

--- InventoryManagementSystem/Controllers/Api/NotificationApiController.cs
+++ InventoryManagementSystem/Controllers/Api/NotificationApiController.cs
@@ -1,4 +1,5 @@
 ﻿using InventoryManagementSystem.Models.EF;
+using InventoryManagementSystem.Models.LINE;
 using InventoryManagementSystem.Models.NotificationModels;
 using InventoryManagementSystem.Models.ResultModels;
 using MailKit.Net.Smtp;
@@ -10,6 +11,8 @@ using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,12 +23,18 @@ namespace InventoryManagementSystem.Controllers.Api
     public class NotificationApiController : ControllerBase
     {
         private readonly InventoryManagementSystemContext _dbContext;
-        private readonly NotificationConfig _config;
+        private readonly NotificationConfig _notificaionConfig;
+        private readonly LineConfig _lineConfig;
+        private static HttpClient Client = new HttpClient();
 
-        public NotificationApiController(InventoryManagementSystemContext dbContext, IOptions<NotificationConfig> config)
+        public NotificationApiController(
+            InventoryManagementSystemContext dbContext, 
+            IOptions<NotificationConfig> notificationConfig, 
+            IOptions<LineConfig> lineConfig)
         {
             _dbContext = dbContext;
-            _config = config.Value;
+            _notificaionConfig = notificationConfig.Value;
+            _lineConfig = lineConfig.Value;
         }
 
         [HttpPost]
@@ -38,7 +47,7 @@ namespace InventoryManagementSystem.Controllers.Api
 
             string authKey = authString.Substring("basic ".Length).Trim();
 
-            if(authKey != _config.ApiKey)
+            if(authKey != _notificaionConfig.ApiKey)
                 return Unauthorized();
 
             var usersWithOverdueOrder = await _dbContext.Users
@@ -61,48 +70,87 @@ namespace InventoryManagementSystem.Controllers.Api
                             EquipmentName = o.Equipment.EquipmentName,
                             Number = o.OrderDetails.Count(od => od.OrderDetailStatusId == "T")
                         })
-
                 })
                 .ToArrayAsync();
 
-
-
-
-            using(var client = new SmtpClient())
+            using(var smtpClient = new SmtpClient())
             {
-                await client.ConnectAsync(_config.Host, _config.Port, false);
-                await client.AuthenticateAsync(_config.User, _config.Pass);
+                await smtpClient.ConnectAsync(_notificaionConfig.Host, _notificaionConfig.Port, false);
+                await smtpClient.AuthenticateAsync(_notificaionConfig.User, _notificaionConfig.Pass);
 
                 foreach(var user in usersWithOverdueOrder)
                 {
+                    #region Build text message
+                    StringBuilder builder = new StringBuilder();
+                    builder.AppendFormat("@{0} 您好：\n\n", user.Username);
+                    foreach(var order in user.OverdueOrders)
+                    {
+                        builder.AppendFormat("您所租借的「{0}」仍有 {1} 筆尚未歸還。\n", order.EquipmentName, order.Number);
+                    }
+                    builder.Append("\n");
+                    builder.Append("請您儘速歸還，感謝您的配合。");
+
+                    string lineText = builder.ToString();
+
+                    builder.Append("<br>");
+                    builder.Replace("\n", "<br>");
+                    builder.Append("本信為系統自動發送，請勿直接回覆此信件。");
+                    builder.Insert(0, "<p>");
+                    builder.Append("</p>");
+                    string emailText = builder.ToString();
+                    #endregion
+
+                    #region Send Email
                     var message = new MimeMessage();
-                    message.From.Add(new MailboxAddress(_config.Name, _config.User));
+                    message.From.Add(new MailboxAddress(_notificaionConfig.Name, _notificaionConfig.User));
                     message.To.Add(new MailboxAddress(user.FullName, user.Email));
 
                     message.Subject = "逾期通知";
 
-                    StringBuilder builder = new StringBuilder();
-                    builder.Append("<p>");
-                    builder.AppendFormat("@{0} 您好：<br><br>", user.Username);
-                    foreach(var order in user.OverdueOrders)
-                    {
-                        builder.AppendFormat("您所租借的「{0}」仍有 {1} 筆尚未歸還。<br>", order.EquipmentName, order.Number);
-                    }
-                    builder.Append("<br>");
-                    builder.Append("請您儘速歸還，感謝您的配合。<br>");
-                    builder.Append("本信為系統自動發送，請勿直接回覆此郵件。");
-                    builder.Append("</p>");
 
                     message.Body = new TextPart("html")
                     {
-                        Text = builder.ToString()
+                        Text = emailText
                     };
 
-                    await client.SendAsync(message);
+                    await smtpClient.SendAsync(message);
+                    #endregion
+
+                    #region Send Line Message
+                    // 有綁定 LINE 才傳送訊息
+                    if(string.IsNullOrWhiteSpace(user.LineId))
+                        continue;
+
+                    PushMessage pushMessage = new PushMessage
+                    {
+                        to = user.LineId,
+                        messages = new[]
+                        {
+                            new LineMessage
+                            {
+                                type = "text",
+                                text = lineText
+                            }
+                        }
+                    };
+
+                    HttpContent content = JsonContent.Create<PushMessage>(pushMessage);
+
+                    HttpRequestMessage request = new HttpRequestMessage();
+                    request.Method = new HttpMethod("POST");
+                    request.RequestUri = new Uri("https://api.line.me/v2/bot/message/push");
+                    request.Headers
+                        .Accept
+                        .Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _lineConfig.AccessToken);
+                    request.Content = content;
+                    await Client.SendAsync(request);
+
+                    #endregion
                 }
 
-                if(client.IsConnected)
-                    await client.DisconnectAsync(true);
+                if(smtpClient.IsConnected)
+                    await smtpClient.DisconnectAsync(true);
             }
             return Ok();
         }
