bb0840ad7c5c689b822938e0b33b3ecfcd1e9fdb
sammiforphoto@gmail.com <sammiforphoto@gmail.com>
2021-08-03 01:11:19 +0800

    ReportApi 修改為可拿全部Reports或是針對單一Report取Json

--- InventoryManagementSystem/Models/ResultModels/GetReportsResultModel.cs
+++ InventoryManagementSystem/Models/ResultModels/GetReportsResultModel.cs
@@ -9,6 +9,8 @@ namespace InventoryManagementSystem.Models.ResultModels
     {
         public Guid ReportId { get; set; }
         public Guid OrderDetailId { get; set; }
+        public int OrderSn { get; set; }
+        public int OrderDetailIdSn { get; set; }
         public string Description { get; set; }
         public DateTime? ReportTime { get; set; }
         public DateTime? CloseTime { get; set; }
