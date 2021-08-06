a59c73f3ef4f0fd62c823863e0241891f0d616f6
Sophia Chen <sophia83124@hotmail.com>
2021-08-06 12:28:05 +0800

    報表"所有租借紀錄" API完成

---
+++ InventoryManagementSystem/Controllers/Api/ReportGenerateApiController.cs
@@ -0,0 +1,81 @@
+﻿using InventoryManagementSystem.Models.EF;
+using InventoryManagementSystem.Models.NotificationModels;
+using InventoryManagementSystem.Models.ResultModels;
+using InventoryManagementSystem.Models.ViewModels;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Controllers.Api
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class ReportGenerateApiController : ControllerBase
+    {
+
+        private readonly InventoryManagementSystemContext _dbContext;
+
+
+        public ReportGenerateApiController(
+            InventoryManagementSystemContext dbContext)
+        {
+            _dbContext = dbContext;
+
+        }
+
+        [HttpGet]
+        [Route("{StartDate}/{EndDate}")]
+        [Produces("application/json")]
+        // 查詢order報表
+        public async Task<IActionResult> GetOrderReport(DateTime StartDate, DateTime EndDate)
+        {
+
+            IQueryable<Order> tempOrders = null;
+            tempOrders = _dbContext.Orders.Select(o => o);
+
+            OrderReportResultModel[] orders = await tempOrders.Select(o => new OrderReportResultModel
+            {
+                OrderTime = o.OrderTime,
+                OrderId = o.OrderId,
+                OrderSn = o.OrderSn,
+                UserId = o.UserId,
+                Username = o.User.Username,
+                EquipmentId = o.EquipmentId,
+                EquipmentSn = o.Equipment.EquipmentSn,
+                EquipmentName = o.Equipment.EquipmentName,
+                Quantity = o.Quantity,
+                EstimatedPickupTime = o.EstimatedPickupTime,
+                OrderStatusId=o.OrderStatusId,
+                OrderDetails = o.OrderDetails.Select(od => new OrderDetailResultModel
+                {
+                    ItemId = od.ItemId,
+                    ItemSn = od.Item.ItemSn,
+                    ItemStatus = od.ItemLogs
+                        .OrderByDescending(il => il.CreateTime)
+                        .Select(il => il.Condition.ConditionName)
+                        .FirstOrDefault()
+                })
+                .ToArray()
+            })
+            .Where(o => o.OrderTime >= StartDate && o.OrderTime <= EndDate)
+            .Where(o => o.OrderStatusId == "E")
+            .ToArrayAsync();
+
+          
+            return Ok(orders);
+
+        }
+
+
+    }
+}
---
+++ InventoryManagementSystem/Models/ResultModels/OrderReportResultModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Models.ResultModels
+{
+    public class OrderReportResultModel
+    {
+
+        public DateTime? OrderTime { get; set; }
+        public Guid OrderId { get; set; }
+        public int OrderSn { get; set; }
+        public Guid UserId { get; set; }
+        public String Username { get; set; }
+        public Guid EquipmentId { get; set; }
+        public String EquipmentSn { get; set; }
+        public String EquipmentName { get; set; }
+        public int Quantity { get; set; }
+        public DateTime EstimatedPickupTime { get; set; }
+        public String OrderStatusId { get; set; }
+        public OrderDetailResultModel[] OrderDetails { get; set; }
+    }
+}
