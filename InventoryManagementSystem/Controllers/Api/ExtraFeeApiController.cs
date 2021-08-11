1bb3003abd05cdcef719c5941799874a414bc1ad
LEE Ngeefah Ian <ionian1hx@gmail.com>
2021-08-11 16:17:27 +0800

    新增額外罰金時會以 Email 或 LINE 通知使用者

--- InventoryManagementSystem/Controllers/Api/ExtraFeeApiController.cs
+++ InventoryManagementSystem/Controllers/Api/ExtraFeeApiController.cs
@@ -1,4 +1,5 @@
 ﻿using InventoryManagementSystem.Models.EF;
+using InventoryManagementSystem.Models.NotificationModels;
 using InventoryManagementSystem.Models.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace InventoryManagementSystem.Controllers.Api
@@ -15,10 +17,12 @@ namespace InventoryManagementSystem.Controllers.Api
     public class ExtraFeeApiController : ControllerBase
     {
         private readonly InventoryManagementSystemContext _dbContext;
+        private readonly NotificationService notificationService;
 
-        public ExtraFeeApiController(InventoryManagementSystemContext dbContext)
+        public ExtraFeeApiController(InventoryManagementSystemContext dbContext, NotificationService notificationService)
         {
             _dbContext = dbContext;
+            this.notificationService = notificationService;
         }
 
         /* method: POST
@@ -68,6 +72,36 @@ namespace InventoryManagementSystem.Controllers.Api
             {
                 return Conflict("更新資料庫失敗");
             }
+
+            var extraFeeInfo = await _dbContext.OrderDetails
+                .Where(od => od.OrderDetailId == model.OrderDetailId)
+                .Select(od => new
+                {
+                    EquipmentName = od.Order.Equipment.EquipmentName,
+                    User = new
+                    {
+                        FullName = od.Order.User.FullName,
+                        UserId = od.Order.UserId,
+                        LineId = od.Order.User.LineId,
+                        Email = od.Order.User.Email,
+                        Username = od.Order.User.Username
+                    }
+                })
+                .FirstOrDefaultAsync();
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append($"@{extraFeeInfo.User.Username} 您好：\n");
+            builder.Append($"您所租借的「{extraFeeInfo.EquipmentName}」產生額外費用新台幣 {model.Fee} 元整。請儘速前往付款。若有任何疑問請聯絡我們，謝謝您的配合。");
+            string lineText = builder.ToString();
+            await notificationService.SendLineNotification(extraFeeInfo.User.LineId, lineText, extraFeeInfo.User.UserId);
+
+            builder.Replace("\n", "<br>");
+            builder.Insert(0, "<p>");
+            builder.Append("</p>");
+            string emailText = builder.ToString();
+            await notificationService.SendEmailNotification(extraFeeInfo.User.FullName, extraFeeInfo.User.Email, "額外費用通知", "html", emailText);
+
+
 
             return Ok();
         }
